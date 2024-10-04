943c6bc14948d1d5f670b1acf5316f0499a41ac8
abhicody <honeydude009@gmail.com>
2024-10-04 12:39:45 +0530

    back to default App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,44 @@
+// import { BrowserRouter } from "react-router-dom";
+
+// import {
+//   About,
+//   Contact,
+//   Experience,
+//   Feedbacks,
+//   Hero,
+//   Navbar,
+//   Tech,
+//   Works,
+//   StarsCanvas,
+// } from "./components";
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       {/* <div className="relative z-0 bg-primary"> */}
+//       <div className="relative z-0 bg-primary">
+//         {/* <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"> */}
+//         <div className="bg-hero-digital bg-cover bg-no-repeat bg-center">
+//           <Navbar />
+//           <Hero />
+//         </div>
+//         <About />
+//         <Experience />
+//         {/* <div className="bg-primary"> */}
+//         <Tech />
+//         <Works />
+//         <Feedbacks />
+//         <div className="relative z-0">
+//           <Contact />
+//           <StarsCanvas />
+//           {/* </div> */}
+//         </div>
+//       </div>
+//     </BrowserRouter>
+//   );
+// };
+
+// export default App;
 import { BrowserRouter } from "react-router-dom";
 
 import {
@@ -15,23 +56,19 @@ import {
 const App = () => {
   return (
     <BrowserRouter>
-      {/* <div className="relative z-0 bg-primary"> */}
       <div className="relative z-0 bg-primary">
-        {/* <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center"> */}
-        <div className="bg-hero-digital bg-cover bg-no-repeat bg-center">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
         </div>
         <About />
         <Experience />
-        {/* <div className="bg-primary"> */}
         <Tech />
         <Works />
         <Feedbacks />
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
-          {/* </div> */}
         </div>
       </div>
     </BrowserRouter>
